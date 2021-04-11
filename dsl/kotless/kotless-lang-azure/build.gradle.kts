0ab001e65e9324bf60a06776034db9670747ba4b
ValchukDmitry <dart.fk712@gmail.com>
2021-04-12 02:56:22 +0300

    Add KotlessAWS to entrypoint classes set

--- dsl/kotless/kotless-lang-azure/build.gradle.kts
+++ dsl/kotless/kotless-lang-azure/build.gradle.kts
@@ -10,7 +10,9 @@ dependencies {
 
     implementation(kotlin("reflect"))
     implementation("org.reflections", "reflections", "0.9.11")
-    implementation("com.microsoft.azure.functions", "azure-functions-java-library", "1.2.2")
+    implementation("com.microsoft.azure.functions", "azure-functions-java-library", "1.2.2") {
+        exclude("org.slf4j", "slf4j-api")
+    }
 
     implementation("ch.qos.logback", "logback-classic", Versions.logback)
 }
